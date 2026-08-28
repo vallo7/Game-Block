a9822b4a29774546ea91916e9e4a46e3bbf3c757
vallo7 <vallotsopna@gmail.com>
2026-08-28 13:08:58 +0100

    Update menu.js

--- www/js/menu.js
+++ www/js/menu.js
@@ -4,12 +4,14 @@ const Menu = {
 
     classicModeBtn.addEventListener("click", () => {
       GameAudio.unlock();
+      GameAudio.playClick();
       Haptics.vibrate(8);
       App.showGame();
     });
 
     document.querySelectorAll(".mode-card.locked").forEach(button => {
       button.addEventListener("click", () => {
+        GameAudio.playClick();
         Haptics.vibrate(10);
       });
     });
