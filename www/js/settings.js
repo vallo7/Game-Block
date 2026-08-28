255f1c4be5ae436c798920713ca8a8e46527bd58
vallo7 <vallotsopna@gmail.com>
2026-08-28 12:09:14 +0100

    Create settings.js

---
+++ www/js/settings.js
@@ -0,0 +1,44 @@
+const Settings = {
+  data: {
+    sound: true,
+    music: false,
+    vibration: true
+  },
+
+  load() {
+    this.data = Storage.getSettings();
+    this.apply();
+    this.updateUI();
+  },
+
+  save() {
+    Storage.saveSettings(this.data);
+  },
+
+  apply() {
+    GameAudio.setSoundEnabled(this.data.sound);
+    GameAudio.setMusicEnabled(this.data.music);
+    Haptics.setEnabled(this.data.vibration);
+  },
+
+  toggle(key) {
+    if (!(key in this.data)) return;
+
+    this.data[key] = !this.data[key];
+
+    this.save();
+    this.apply();
+    this.updateUI();
+
+    if (key === "music" && this.data.music) {
+      GameAudio.unlock();
+    }
+  },
+
+  updateUI() {
+    document.querySelectorAll("[data-setting]").forEach(button => {
+      const key = button.dataset.setting;
+      button.classList.toggle("on", Boolean(this.data[key]));
+    });
+  }
+};
