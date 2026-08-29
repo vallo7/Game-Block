425a78d37fa9d13e385605a1879e1141a053787e
vallo7 <vallotsopna@gmail.com>
2026-08-29 15:06:01 +0100

    Update settings.js

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,48 +1,36 @@
 const Settings = {
-  data: {
-    sound: true,
-    music: false,
-    vibration: true
-  },
-
-  load() {
-    this.data = Storage.getSettings();
-    this.apply();
-    this.updateUI();
-  },
-
-  save() {
-    Storage.saveSettings(this.data);
-  },
-
-  apply() {
-    GameAudio.setSoundEnabled(this.data.sound);
-    GameAudio.setMusicEnabled(this.data.music);
-    Haptics.setEnabled(this.data.vibration);
-  },
-
-  toggle(key) {
-    if (!(key in this.data)) return;
-
-    this.data[key] = !this.data[key];
-
-    this.save();
-    this.apply();
-    this.updateUI();
-
-    if (key === "vibration" && this.data.vibration) {
-      Haptics.vibrate(300);
-    }
-
-    if (key === "music" && this.data.music) {
-      GameAudio.unlock();
-    }
-  },
-
-  updateUI() {
-    document.querySelectorAll("[data-setting]").forEach(button => {
-      const key = button.dataset.setting;
-      button.classList.toggle("on", Boolean(this.data[key]));
-    });
-  }
+data: {
+sound: true,
+music: false,
+vibration: true,
+noAds: false // Ajout
+},
+load() {
+this.data = Storage.getSettings();
+this.apply();
+this.updateUI();
+},
+save() {
+Storage.saveSettings(this.data);
+},
+apply() {
+GameAudio.setSoundEnabled(this.data.sound);
+GameAudio.setMusicEnabled(this.data.music);
+Haptics.setEnabled(this.data.vibration);
+},
+toggle(key) {
+if (!(key in this.data)) return;
+this.data[key] = !this.data[key];
+this.save();
+this.apply();
+this.updateUI();
+if (key === "vibration" && this.data.vibration) { Haptics.vibrate(300); }
+if (key === "music" && this.data.music) { GameAudio.unlock(); }
+},
+updateUI() {
+document.querySelectorAll("[data-setting]").forEach(button => {
+const key = button.dataset.setting;
+button.classList.toggle("on", Boolean(this.data[key]));
+});
+}
 };
